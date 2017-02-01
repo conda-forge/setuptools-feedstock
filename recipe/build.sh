64e795bf37610cca7f488c5df69863601deba99a
Jonathan Helmus <jjhelmus@gmail.com>
2017-02-01 09:48:00 -0600

    New requirements and build process for version 34
    Uses a bootstap version of pip to run the bootstrap.py script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# we need pip to install setuptools but do not want to include it as a build
+# requirement as that will pull in setuptools.  Instead use an bootstrap
+# install of pip and include the directory in the python search path.
+wget -q https://pypi.io/packages/source/p/pip/pip-9.0.1.tar.gz
+tar xfz pip-9.0.1.tar.gz
+PIP_DIR="$SRC_DIR/pip-9.0.1"
+
 export SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT=0
 export DISTRIBUTE_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT=0
 
-$PYTHON bootstrap.py
+PYTHONPATH=$PIP_DIR $PYTHON bootstrap.py
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
