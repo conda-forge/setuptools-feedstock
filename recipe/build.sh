5939341f22fed33bb3f4fa3241f92bab0543a279
Jonathan Helmus <jjhelmus@gmail.com>
2017-02-01 12:10:20 -0600

    Use a mock version of pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# we need pip to install setuptools but do not want to include it as a build
-# requirement as that will pull in setuptools.  Instead use an bootstrap
-# install of pip and include the directory in the python search path.
-wget -q https://pypi.io/packages/source/p/pip/pip-9.0.1.tar.gz
-tar xfz pip-9.0.1.tar.gz
-PIP_DIR="$SRC_DIR/pip-9.0.1"
+# the bootstrap.py script requires pip to install dependencies. conda is
+# providing these dependencies and therefore pip is not needed. Copy a mock
+# version of the pip module so that the bootstrap.py script can be run.
+cp $RECIPE_DIR/mock_pip.py pip.py
 
 export SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT=0
 export DISTRIBUTE_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT=0
 
-PYTHONPATH=$PIP_DIR $PYTHON bootstrap.py
+$PYTHON bootstrap.py
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
---
+++ recipe/mock_pip.py
@@ -0,0 +1,4 @@
+# Mock version of pip required for bootstrap.py script
+
+def main(args):
+    pass
