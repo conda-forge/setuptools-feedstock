9f02375e5b812072be5b9cceb119acb16ba2e73f
Jonathan Helmus <jjhelmus@gmail.com>
2017-02-01 12:17:33 -0600

    Add /Y flag to copy command in windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 :: the bootstrap.py script requires pip to install dependencies. conda is
 :: providing these dependencies and therefore pip is not needed. Copy a mock
 :: version of the pip module so that the bootstrap.py script can be run.
-copy %RECIPE_DIR%\mock_pip.py pip.py
+copy /Y %RECIPE_DIR%\mock_pip.py pip.py
 
 set SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES=0
 set SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT=0
